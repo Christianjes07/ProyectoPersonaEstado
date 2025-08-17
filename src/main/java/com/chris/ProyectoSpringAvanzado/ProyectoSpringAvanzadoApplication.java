29f1cd51f630110009a4b6841c57c8490856d0b1
Christianjes07 <jesussaucedo896@gmail.com>
2025-08-17 12:36:27 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/chris/ProyectoSpringAvanzado/ProyectoSpringAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.chris.ProyectoSpringAvanzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringAvanzadoApplication.class, args);
+	}
+
+}
